89cbdd853e4fbc12d8cfcc4039e6f218b8180dec
12abhiahek <abhisheks141999@gmail.com>
2024-06-10 14:58:48 +0530

    hotel class are created

---
+++ userDetailsService/src/main/java/com/example/microservice/userDetailsService/Entity/Hotel.java
@@ -0,0 +1,71 @@
+package com.example.microservice.userDetailsService.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="Hotel")
+public class Hotel {
+
+    @Id
+    @Column
+    private String id;
+    @Column
+    private String name;
+    @Column
+    private String location;
+    @Column
+    private String about;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", about='" + about + '\'' +
+                '}';
+    }
+}
